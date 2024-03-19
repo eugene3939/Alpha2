33555aecb1b6f0cd11c0f13c1f97721679980471
2301081 <2301081@systex.com.tw>
2024-03-19 16:21:46 +0800

    user dao create setup

--- app/src/main/java/com/example/alpha2/DBManager/Product/Product.kt
+++ app/src/main/java/com/example/alpha2/DBManager/Product/Product.kt
@@ -1,4 +1,3 @@
 package com.example.alpha2.DBManager.Product
-
-class Product {
+ class Product {
 }
--- app/src/main/java/com/example/alpha2/Login.kt
+++ app/src/main/java/com/example/alpha2/Login.kt
@@ -2,10 +2,47 @@ package com.example.alpha2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.example.alpha2.DBManager.User.User
+import com.example.alpha2.DBManager.User.UserManager
+import com.example.alpha2.databinding.ActivityLoginBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var userDBManager: UserManager       //用戶Dao (用封裝的方式獲取Dao)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        // 使用 View Binding 初始化綁定
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 初始化資料庫管理器
+        userDBManager = UserManager(applicationContext)
+
+        //整檔
+        insertUserDB("1","Eugene", "1", "1", "clerk")        //建立預設用戶
+        insertUserDB("2","Oscar", "3", "3","clerk2")
+    }
+
+    //新增user dao Table
+    private fun insertUserDB(id: String,name: String,account: String,password: String,authentication: String) {
+        lifecycleScope.launch(Dispatchers.IO){
+            //確認用戶是否已經存在
+            val existingUser = userDBManager.getUserById(id)
+            if (existingUser == null) {
+                val user = User(id, name, account,password,authentication)
+                userDBManager.addUser(user)
+                Log.d("新增用戶", "User added: $user")
+            } else {    //確認是否為已知id
+                Log.d("既有用戶", "User with ID $id already exists")
+            }
+        }
     }
 }
