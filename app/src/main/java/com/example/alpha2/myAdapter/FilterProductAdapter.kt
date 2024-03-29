1043358cc6464e7af7cbcdac81976e55d61aa3b8
2301081 <2301081@systex.com.tw>
2024-03-29 11:51:32 +0800

    selectItem change reset show up

--- app/src/main/java/com/example/alpha2/myAdapter/FilterProductAdapter.kt
+++ app/src/main/java/com/example/alpha2/myAdapter/FilterProductAdapter.kt
@@ -28,35 +28,35 @@ class FilterProductAdapter(private val dataList: List<Product>,private val numbe
 
         if (view == null) {
             view = LayoutInflater.from(parent?.context).inflate(R.layout.selectitem, parent, false)
-            holder = ViewHolder(view, numberInf)
+            holder = ViewHolder(view, numberInf, position+1)
             view.tag = holder
         } else {
             holder = view.tag as ViewHolder
         }
 
         val product = dataList[position]
-        holder.bind(product, numberInf)
+        holder.bind(product, numberInf, position + 1)
 
         return view!!
     }
 
-    class ViewHolder(itemView: View, numberInf: MutableMap<Product, Int>) {
+    class ViewHolder(itemView: View, numberInf: MutableMap<Product, Int>, productId: Int) {
         //請見selectitem.xml
+        private val productID: TextView = itemView.findViewById(R.id.txt_productIndex)
         private val productName: TextView = itemView.findViewById(R.id.txt_productName)
-        private val productType: TextView = itemView.findViewById(R.id.txt_productType)
         private val productPrice: TextView = itemView.findViewById(R.id.txt_productPrice)
-        private val productNumber: TextView = itemView.findViewById(R.id.txt_productNumber)
+        private val productSum: TextView = itemView.findViewById(R.id.txt_productSum)
 
         private val shopNumber: TextView = itemView.findViewById(R.id.txt_product_buy_number)
         @SuppressLint("SetTextI18n")
-        fun bind(product: Product, numberInf: MutableMap<Product, Int>) {
+        fun bind(product: Product, numberInf: MutableMap<Product, Int>, ID: Int) {
+            productID.text = "$ID"
             // 使用 Glide 或其他圖片載入庫載入商品圖片
             shopNumber.text = "x ${numberInf[product]}"
 
             productName.text = truncateString(product.pName, 20)
-            productType.text = product.pType
-            productPrice.text = "${product.unitPrc}元"
-            productNumber.text = "${product.pNumber}個"
+            productPrice.text = "單價: ${product.unitPrc} 元"
+            productSum.text = "小計: ${product.unitPrc * numberInf[product]!!} 元"     //單向小計
         }
 
         //限制字串長度
--- app/src/main/java/com/example/alpha2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/alpha2/ui/home/HomeFragment.kt
@@ -2,7 +2,6 @@ package com.example.alpha2.ui.home
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -16,7 +15,6 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
-import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.alpha2.DBManager.Product.Product
